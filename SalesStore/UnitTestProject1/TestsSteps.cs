adb8f7e59943ca02108113abe6440846536c068b
Alejandro-Frech <alefrech@gmail.com>
2016-06-24 22:49:50 -0600

    products and inventory data

--- SalesStore/UnitTestProject1/TestsSteps.cs
+++ SalesStore/UnitTestProject1/TestsSteps.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 
 namespace UnitTestProject1
@@ -21,6 +22,13 @@ namespace UnitTestProject1
         [Given(@"There is enough inventory")]
         public void GivenThereIsEnoughInventory()
         {
+            var products = new List<Product>()
+            {
+                new Product("001", "USB", "2 GB Black", 99.98f),
+                new Product("002", "Monitor", "40 pulgadas", 350.95f),
+                new Product("003", "SDD", "1 TB Black", 498f)
+            };
+
             ScenarioContext.Current.Pending();
         }
         
@@ -39,6 +47,13 @@ namespace UnitTestProject1
         [Then(@"We subtract the quantity buyed")]
         public void ThenWeSubtractTheQuantityBuyed(Table table)
         {
+            var inventoryOperations = new List<InventoryOperation>()
+            {
+                new InventoryOperation(1,5,"Purchase",DateTime.Now),
+                new InventoryOperation(2,5,"Purchase",DateTime.Now),
+                new InventoryOperation(3,5,"Purchase",DateTime.Now)
+            };
+
             ScenarioContext.Current.Pending();
         }
     }
